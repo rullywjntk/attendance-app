fc2169c1b6a677b26c6f4e6f04e58b094ab39b49
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-07-19 08:25:57 +0700

    ui: login page

---
+++ app/src/main/java/com/rullywjntk/attendanceapp/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.rullywjntk.attendanceapp.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rullywjntk.attendanceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
